3f5011b5e7d04f2af3d32b616095e6151f64aa2c
RetroScript <andrefrancelino06@gmail.com>
2023-08-12 11:55:02 -0300

    Project import - initial commit
    Importing the project files right after creating the repository.

---
+++ OPERAÇÕES MATEMÁTICAS COM MÉTODOS - CSHARP project/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        Program p = new Program();
+        Console.WriteLine(p.subtraction(6, 3) +  Console.ReadLine());
+
+       
+    }
+
+    
+
+    public int subtraction(int number1, int number2){
+        int result = number1 - number2;
+        return result;
+    }
+
+}
