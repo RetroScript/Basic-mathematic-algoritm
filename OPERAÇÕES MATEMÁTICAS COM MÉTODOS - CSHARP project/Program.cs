40e472fe44d1936c92e361ffa6383893a9382206
RetroScript <andrefrancelino06@gmail.com>
2023-08-12 13:19:43 -0300

    More operations added
    methods whose describe more operations were added to the code, the new operations are: addition, multiplication, and division.
    
    novos métodos que descrevem mais operações foram adicionados ao código, as novas operações são:
    adição, multiplicação, e divisão.

--- OPERAÇÕES MATEMÁTICAS COM MÉTODOS - CSHARP project/Program.cs
+++ OPERAÇÕES MATEMÁTICAS COM MÉTODOS - CSHARP project/Program.cs
@@ -1,4 +1,6 @@
-﻿internal class Program
+﻿using System.Xml.XPath;
+
+internal class Program
 {
 
     private static void Main(string[] args)
@@ -6,16 +8,34 @@
         Console.WriteLine("Hello, World!");
 
         Program p = new Program();
-        Console.WriteLine(p.subtraction(6, 3) +  Console.ReadLine());
-
+        Console.WriteLine(p.subtraction(6, 2));
+        Console.WriteLine(p.addition(2, 2));
+        Console.WriteLine(p.multiplication(2,2));
+        Console.WriteLine(p.division(8,2));
        
     }
 
-    
+
+
+    public int addition(int number1, int number2){
+        int result = number1 + number2;
+        return result;
+    }
 
     public int subtraction(int number1, int number2){
         int result = number1 - number2;
         return result;
+    
     }
+
+    public int multiplication(int number1, int number2){
+        int result = number1 * number2;
+        return result;
+    }
+
+    public int division(int number1, int number2){
+        int result = number1 / number2;
+        return result;
+    }    
 
 }
