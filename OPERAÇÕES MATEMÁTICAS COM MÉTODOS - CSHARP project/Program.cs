e31850497362a6f8b5f7e570e73f777ab8dccdbd
RetroScript <andrefrancelino06@gmail.com>
2023-08-16 17:22:59 -0300

    Simple user interactions added
    added code responsible to receive an user input and check wich operation was chose, user cannot determinate the values to the operations yet.
    
    código responsável pela entrada de dados e pela checagem da operação escolhida adicionado, o usuário ainda não pode determinar os valores que serão usados nas operações.

--- OPERAÇÕES MATEMÁTICAS COM MÉTODOS - CSHARP project/Program.cs
+++ OPERAÇÕES MATEMÁTICAS COM MÉTODOS - CSHARP project/Program.cs
@@ -3,19 +3,46 @@
 internal class Program
 {
 
+    public static String? userInput;
+
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        
+        Console.WriteLine("which operation you want to make?");
+        Console.WriteLine("addition => +");
+        Console.WriteLine("subtraction => -");
+        Console.WriteLine("multiplication => *");
+        Console.WriteLine("division => /");
+
+        userInput = Console.ReadLine();
 
         Program p = new Program();
-        Console.WriteLine(p.subtraction(6, 2));
-        Console.WriteLine(p.addition(2, 2));
-        Console.WriteLine(p.multiplication(2,2));
-        Console.WriteLine(p.division(8,2));
+
+        switch (userInput) {
+            case "+":
+            Console.WriteLine(p.addition(2, 2));
+            break;
+            
+            case "-":
+            Console.WriteLine(p.subtraction(6, 2));
+            break;
+
+            case "*":
+            Console.WriteLine(p.multiplication(2,2));
+            break;
+
+            case "/":
+            Console.WriteLine(p.division(8,2));
+            break;
+        }
+        
+
+        
+        
+
        
     }
-
-
 
     public int addition(int number1, int number2){
         int result = number1 + number2;
